6b6c52207178cdb674ffb7d20ab7d1d1736a63cf
webchick <webchick@24967.no-reply.drupal.org>
2013-11-30 12:58:52 -0800

    Put in API call to GrooveShark (Hey, something's kinda working! :))

--- lib/Drupal/grooveshart/Controller/GrooveShartController.php
+++ lib/Drupal/grooveshart/Controller/GrooveShartController.php
@@ -16,9 +16,19 @@ class GrooveShartController {
    * Streams a truly terrible song from the GrooveShark API.
    */
   public function stream() {
+    $client = \Drupal::httpClient();
+
+    // @todo Grab a random value from config file.
+    $terrible_song = urlencode('Pauly Shore Lisa Lisa');
+
+    // @todo Get API key from config.
+    $api_key = '';
+    $request = $client->get("http://tinysong.com/b/$terrible_song/?format=json&key=$api_key");
+
+    $response = $request->send();
+
     return array(
-      '#markup' => t('TODO'),
+      '#markup' => debug($response->json()),
     );
   }
-
 }
